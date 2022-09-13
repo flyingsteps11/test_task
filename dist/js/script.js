d64e0a2eae7c82aec3b798e2ec9c6e4bb986fac8
mvovk <mvovk@wr.company>
2022-09-13 21:44:19 +0300

    final commit

--- dist/js/script.js
+++ dist/js/script.js
@@ -12,60 +12,30 @@ $(document).ready(function () {
         $(this).toggleClass('active');
         $(this).find('.dropdown-menu').slideToggle(300);
     });
+
     $('.dropdown').focusout(function () {
         $(this).removeClass('active');
         $(this).find('.dropdown-menu').slideUp(300);
     });
+
     $('.dropdown .dropdown-menu li').click(function () {
         $(this).parents('.dropdown').find('span').text($(this).text());
         $(this).parents('.dropdown').find('input').attr('value', $(this).attr('id'));
     });
-    /*End Dropdown Menu*/
-
 
     $('.dropdown-menu li').click(function () {
         var input = '<strong>' + $(this).parents('.dropdown').find('input').val() + '</strong>',
             msg = '<span class="msg">Hidden input value: ';
         $('.msg').html(msg + input + '</span>');
     });
-    const scrollElements = document.querySelectorAll(".js-scroll");
 
-    const elementInView = (el, dividend = 1) => {
-        const elementTop = el.getBoundingClientRect().top;
-
-        return (
-            elementTop <=
-            (window.innerHeight || document.documentElement.clientHeight) / dividend
-        );
-    };
-
-    const elementOutofView = (el) => {
-        const elementTop = el.getBoundingClientRect().top;
-
-        return (
-            elementTop > (window.innerHeight || document.documentElement.clientHeight)
-        );
-    };
-
-    const displayScrollElement = (element) => {
-        element.classList.add("scrolled");
-    };
-
-    const hideScrollElement = (element) => {
-        element.classList.remove("scrolled");
-    };
-
-    const handleScrollAnimation = () => {
-        scrollElements.forEach((el) => {
-            if (elementInView(el, 1.25)) {
-                displayScrollElement(el);
-            } else if (elementOutofView(el)) {
-                hideScrollElement(el)
-            }
-        })
-    }
-
-    window.addEventListener("scroll", () => {
-        handleScrollAnimation();
-    });
+    $("#menu__toggle").on("click", function () {
+        if ($(this).is(":checked")) {
+            document.body.style.overflowY = 'hidden';
+            document.querySelector('html').style.overflowY = 'hidden';
+        } else {
+            document.body.style.overflowY = 'auto';
+            document.querySelector('html').style.overflowY = 'auto'
+        }
+    })
 });
